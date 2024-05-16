bb694b567af68c8295c5f370b65c02867356c553
Boofdev <hexahigh0@gmail.com>
2024-05-16 22:38:00 +0200

    added sanitization to the log

--- util.go
+++ util.go
@@ -1,13 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func verbosePrintln(minLevel int, msg ...any) {
 	if *params.Verbosity >= minLevel {
+		msg = sanitize(msg...)
 		msg = append([]any{"[" + verbosityMap[minLevel] + "]"}, msg...)
 		logger.Println(msg...)
 	}
 }
 
 func verbosePrintf(minLevel int, format string, msg ...any) {
+	msg = sanitize(msg...)
 	msg = append([]any{"[" + verbosityMap[minLevel] + "]"}, msg...)
 	logger.Printf(format, msg...)
+}
+
+func sanitize(msg ...any) []any {
+	// Check if the message contains the token
+	if strings.Contains(fmt.Sprint(msg...), *params.Token) {
+		// Replace the token with ***
+		return []any{strings.Replace(fmt.Sprint(msg...), *params.Token, "***", -1)}
+	}
+	return msg
 }
